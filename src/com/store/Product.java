3e7887c5e23591cffae3b1fc801436fd8118ae3e
Thomas Sonderman <trsonderm@gmail.com>
2015-11-15 13:00:42 -0500

    begin adding unit tests

--- src/com/store/Product.java
+++ src/com/store/Product.java
@@ -6,10 +6,10 @@ import java.io.Serializable;
  * Created by WPTRS on 11/4/2015.
  */
 public class Product implements Serializable{
-    int sku;
-    String name;
-    String description;
-    double price;
-    double cost;
-    int quantity;
+    public int sku;
+    public String name;
+    public String description;
+    public double price;
+    public double cost;
+    public int quantity;
 }
